7bf8bb9b52ddd508e754785d8f2399f5d473c23b
michaelryandinio <michaelryandinio@gmail.com>
2015-11-14 13:29:34 -0800

    Merge with branch Mike

--- WindowsApplication2/Form1.Designer.vb
+++ WindowsApplication2/Form1.Designer.vb
@@ -38,7 +38,7 @@ Partial Class Form1
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(60, 130)
+        Me.CheckBox1.Location = New System.Drawing.Point(125, 118)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
         Me.CheckBox1.TabIndex = 1
